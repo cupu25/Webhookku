c248b8d0fe91f3321de923dbe937b8f087007190
Candra Putra Negara <49068417+cupu25@users.noreply.github.com>
2023-04-09 23:50:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import json, config
 from flask import Flask, request, jsonify, render_template
-from binance.cm_futures import CMFutures
-from binance.lib.utils import config_logging
-from binance.error import ClientError
 from binance.client import Client
 from binance.enums import *
 import ssl
@@ -12,8 +9,9 @@ import requests
 app = Flask(__name__)
 
 
-API_KEY = 'DV5uON51MNenChwzxHywR03HBWyT2ncWCVxljs3cdS6WvveTqmaiZyqTqdOi2I7f'
-API_SECRET = 'TkJUUld24MAtobhuJT9jReFW8wkXoPq5E14D8k14rCLg2S5eB8CrodfNJRguokbO'
+API_KEY = "DV5uON51MNenChwzxHywR03HBWyT2ncWCVxljs3cdS6WvveTqmaiZyqTqdOi2I7f"
+API_SECRET = "TkJUUld24MAtobhuJT9jReFW8wkXoPq5E14D8k14rCLg2S5eB8CrodfNJRguokbO"
+
 client = Client(API_KEY, API_SECRET)
 
 def order(symbol, side, quantity):
